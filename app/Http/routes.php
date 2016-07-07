fc290e722ed526c4927fe421cc2af78f3c4be007
Chaow Porkaew <chaow.po@up.ac.th>
2016-07-07 15:53:44 +0700

    ggg

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,25 +1,12 @@
 <?php
 
-use App\Task;
-use Illuminate\Http\Request;
 
 /**
  * Show Task Dashboard
  */
 Route::get('/', function () {
-    return view('tasks');
-});
+    $tasks = \App\Task::all();
 
-/**
- * Add New Task
- */
-Route::post('/task', function (Request $request) {
-    //
-});
-
-/**
- * Delete Task
- */
-Route::delete('/task/{task}', function (Task $task) {
-    //
+    return view('tasks')
+        ->with("taskLists",$tasks);
 });
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,1 +1,29 @@
-Hello World
+<html>
+<head>
+    <title>Task Management</title>
+</head>
+<body>
+
+<h1>Task Management</h1>
+
+<table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($taskLists as $task)
+    <tr>
+        <td>{{$task->id}}</td>
+        <td>{{$task->name}}</td>
+        <td>Action</td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
+
+</body>
+</html>
