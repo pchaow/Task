5f6bf8156cc447bc0b4db0c27897ddc3858de9f1
Chaow Porkaew <chaow.po@up.ac.th>
2016-07-08 13:13:08 +0700

    something

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,5 +8,24 @@ Route::get('/', function () {
     $tasks = \App\Task::all();
 
     return view('tasks')
-        ->with("taskLists",$tasks);
+        ->with("taskLists", $tasks);
 });
+
+
+Route::post('/add-task', function (\Illuminate\Http\Request $request) {
+    $form = $request->get('task');
+
+    $task = new \App\Task();
+    $task->name = $form['name'];
+    $task->save();
+
+    return redirect('/');
+});
+
+
+
+
+
+
+
+
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,6 +6,13 @@
 
 <h1>Task Management</h1>
 
+<form method="post" action="/add-task">
+    {{csrf_field()}}
+    <label>Name : </label>
+    <input type="text" name="task[name]"/>
+    <button type="submit">Add</button>
+</form>
+
 <table>
     <thead>
     <tr>
@@ -16,11 +23,11 @@
     </thead>
     <tbody>
     @foreach($taskLists as $task)
-    <tr>
-        <td>{{$task->id}}</td>
-        <td>{{$task->name}}</td>
-        <td>Action</td>
-    </tr>
+        <tr>
+            <td>{{$task->id}}</td>
+            <td>{{$task->name}}</td>
+            <td>Action</td>
+        </tr>
     @endforeach
     </tbody>
 </table>
